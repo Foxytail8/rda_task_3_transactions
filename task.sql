a8ee77c7534583d6c041b06460f0cc10c61192d2
Foxytail8 <xinenax@gmail.com>
2026-06-07 17:32:38 +0300

    'Solution'

--- task.sql
+++ task.sql
@@ -6,6 +6,9 @@ USE ShopDB;
 -- Start the transaction 
 START TRANSACTION; 
 
--- And some data should be created inside the transaction 
+INSERT INTO Orders (CustomerID, Date) VALUES (1, '2023-01-01');
+INSERT INTO OrderItems (OrderID, ProductID, Count) 
+VALUES (LAST_INSERT_ID(), 1, 1);
+UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
 
 COMMIT; 
